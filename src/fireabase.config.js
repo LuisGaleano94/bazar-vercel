ee1375072290825928658708dd2533caba3868e8
Luis Galeano <gal-1994@hotmail.com>
2023-04-24 12:11:11 -0500

    first commit

---
+++ src/fireabase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjb7jtLfaQX2cL9TJYmDJzgoXJ_DveD8s",
+  authDomain: "bazarapp-98931.firebaseapp.com",
+  projectId: "bazarapp-98931",
+  storageBucket: "bazarapp-98931.appspot.com",
+  messagingSenderId: "854297651950",
+  appId: "1:854297651950:web:1ccb57a8d549cf8a97a1ff"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
